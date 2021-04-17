35c34fa1773fc14438bb658b49772041b97319d5
viniciusraja <56417695+viniciusraja@users.noreply.github.com>
2021-04-17 15:12:41 -0300

    added statusBar with custom styles

--- App/router/router.tsx
+++ App/router/router.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {Provider} from 'react-redux';
+import Constants from '../config';
 import configureStore from '../ducks/configurationStore';
 import HomeStack from '../screens/HomeStack/navigation';
 
@@ -8,6 +10,7 @@ const AppNavigator = createAppContainer(HomeStack);
 const store = configureStore();
 const App = () => (
   <Provider store={store}>
+    <StatusBar backgroundColor={Constants.colors.primary} barStyle="light-content"/>
     <AppNavigator />
   </Provider>
 );
