f5b38841dce9d09a88455ae3b3cd198b0e58ced2
Vinícius Rajá <viniciusrfl@hotmail.com>
2021-04-17 16:01:05 -0300

    Added splashScreen

--- App/router/router.tsx
+++ App/router/router.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import {createAppContainer} from 'react-navigation';
 import {Provider} from 'react-redux';
 import Constants from '../config';
@@ -8,11 +9,19 @@ import HomeStack from '../screens/HomeStack/navigation';
 
 const AppNavigator = createAppContainer(HomeStack);
 const store = configureStore();
-const App = () => (
-  <Provider store={store}>
-    <StatusBar backgroundColor={Constants.colors.primary} barStyle="light-content"/>
-    <AppNavigator />
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+  return (
+    <Provider store={store}>
+      <StatusBar
+        backgroundColor={Constants.colors.primary}
+        barStyle="light-content"
+      />
+      <AppNavigator />
+    </Provider>
+  );
+};
 
 export default App;
