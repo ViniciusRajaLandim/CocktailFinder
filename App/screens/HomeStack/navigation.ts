677c92f3aecfa8a9436ae79d6791e8f6742a7e2d
viniciusraja <56417695+viniciusraja@users.noreply.github.com>
2021-04-17 15:15:04 -0300

    Added custom transition to navigation bottom-top

--- App/screens/HomeStack/navigation.ts
+++ App/screens/HomeStack/navigation.ts
@@ -1,4 +1,4 @@
-import {createStackNavigator} from 'react-navigation-stack';
+import { createStackNavigator ,TransitionPresets} from 'react-navigation-stack';
 import HomeScreen from './HomeScreen';
 import SearchingCocktailsScreen from './SearchingCocktailsScreen';
 
@@ -6,16 +6,22 @@ const HomeStack = createStackNavigator({
   HomeScreen: {
     screen: HomeScreen,
 
-    navigationOptions: ({navigation}) => ({
+    navigationOptions: ({ navigation }) => ({
       headerShown: false,
     }),
   },
   SearchingCocktailsScreen: {
     screen: SearchingCocktailsScreen,
 
-    navigationOptions: ({navigation}) => ({
+    navigationOptions: ({ navigation }) => ({
       headerShown: false,
     }),
   },
-});
+},
+{
+    defaultNavigationOptions: {
+      ...TransitionPresets.ModalSlideFromBottomIOS
+    }
+  }
+);
 export default HomeStack;
